f88dc0742de34d1d13880cd81b70b3b71c93e8ce
kaniadhea <kaniadhea7@gmail.com>
2025-01-24 08:14:11 +0700

    menyelesaikan update pada kursus

--- app/src/main/java/com/example/projectakhir/ui/View/Kursus/UpdateKursusView.kt
+++ app/src/main/java/com/example/projectakhir/ui/View/Kursus/UpdateKursusView.kt
@@ -1,2 +1,78 @@
 package com.example.projectakhir.ui.View.Kursus
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectakhir.PenyediaViewModel
+import com.example.projectakhir.ui.ViewModel.Kursus.UpdateKursusViewModel
+import com.example.projectakhir.ui.ViewModel.Kursus.toKur
+import com.example.projectakhir.ui.navigasi.CostumeTopAppBar
+import com.example.projectakhir.ui.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
+
+object DestinasiUpdateKursus : DestinasiNavigasi {
+    override val route = "update"
+    const val ID_Kursus = "nim"
+    val routesWithArg = "$route/{$ID_Kursus}"
+    override val titleRes = "Update Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateView(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateKursusViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val uiState = viewModel.uiState.value
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdateKursus.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            // Pass the UI state to the EntryBody
+            EntryBodyKursus(
+                insertKursusUiState = uiState,
+                onSiswaValueChange = { updatedKursusValue ->
+                    viewModel.updateKurState(updatedKursusValue) // Update ViewModel state
+                },
+                onSaveClick = {
+                    uiState.insertKursusUiEvent?.let { insertKursusUiEvent ->
+                        coroutineScope.launch {
+                            viewModel.updateKur(
+                                id_kursus = viewModel.id_kursus,
+                                kursus = insertKursusUiEvent.toKur()
+                            )
+                            navigateBack() // Navigate back after saving
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/projectakhir/ui/ViewModel/Kursus/UpdateKursusViewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/ViewModel/Kursus/UpdateKursusViewModel.kt
@@ -1,2 +1,57 @@
 package com.example.projectakhir.ui.ViewModel.Kursus
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectakhir.Repository.KursusRepository
+import com.example.projectakhir.model.Kursus
+import com.example.projectakhir.ui.View.Kursus.DestinasiUpdateKursus
+import kotlinx.coroutines.launch
+
+class UpdateKursusViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val kur: KursusRepository
+) : ViewModel(){
+
+    val id_kursus: String = checkNotNull(savedStateHandle[DestinasiUpdateKursus.ID_Kursus])
+
+    var uiState = mutableStateOf(InsertKursusUiState())
+        private set
+    init {
+        ambilKursus()
+    }
+
+
+    private fun ambilKursus() {
+        viewModelScope.launch {
+            try {
+                val kursus = kur.getKursusbyid_kursus(id_kursus)
+                kursus?.let {
+                    uiState.value = it.toInsertKursusUIEvent()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    fun updateKur(id_kursus: String, kursus: Kursus) {
+        viewModelScope.launch {
+            try {
+                kur.updateKursus(id_kursus, kursus)
+
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateKurState(insertKursusUiEvent: InsertKursusUiEvent) {
+        uiState.value = uiState.value.copy(insertKursusUiEvent = insertKursusUiEvent)
+    }
+}
+
+fun Kursus.toInsertKursusUIEvent(): InsertKursusUiState = InsertKursusUiState(
+    insertKursusUiEvent = this.toDetailKursusUiEvent())
+
