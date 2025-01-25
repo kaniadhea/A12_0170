a18095ed4a12654cbe45b47a2952db7f897611e9
kaniadhea <kaniadhea7@gmail.com>
2025-01-25 16:32:51 +0700

    membuat InsertPendaftaranViewModel.kt

--- app/src/main/java/com/example/projectakhir/ui/ViewModel/Pendaftaran/InsertPendaftaranViewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/ViewModel/Pendaftaran/InsertPendaftaranViewModel.kt
@@ -1,2 +1,62 @@
 package com.example.projectakhir.ui.ViewModel.Pendaftaran
 
+import com.example.projectakhir.model.Pendaftaran
+import kotlin.math.tan
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectakhir.Repository.PendaftaranRepository
+import kotlinx.coroutines.launch
+
+
+class InsertPendaftaranViewModel(private val pndftrn: PendaftaranRepository): ViewModel() {
+    var uiState by mutableStateOf(InsertPendaftaranUiState())
+        private set
+
+    fun updateInsertPndftrnState(insertPendftaranUiEvent: InsertPendaftranUiEvent){
+        uiState = InsertPendaftaranUiState(insertPendftaranUiEvent = insertPendftaranUiEvent)
+    }
+
+    suspend fun insertPndftrn() {
+        viewModelScope.launch {
+            try {
+                pndftrn.insertPendaftaran(uiState.insertPendftaranUiEvent.toPndftran())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+
+data class InsertPendaftaranUiState(
+    val insertPendftaranUiEvent: InsertPendaftranUiEvent = InsertPendaftranUiEvent()
+)
+
+data class InsertPendaftranUiEvent(
+
+    val id_pendaftaran: String = "",
+    val id_kursus: String = "",
+    val id_siswa: String = "",
+    val tanggal_pendaftaran: String = ""
+)
+
+fun InsertPendaftranUiEvent.toPndftran(): Pendaftaran = Pendaftaran(
+    id_pendaftaran = id_pendaftaran,
+    id_kursus = id_kursus,
+    id_siswa = id_siswa,
+    tanggal_pendaftaran = tanggal_pendaftaran
+)
+
+fun Pendaftaran.toUiStatePndftrn(): InsertPendaftaranUiState = InsertPendaftaranUiState(
+    insertPendftaranUiEvent = toInsertPndftrnUiEvent()
+)
+
+fun Pendaftaran.toInsertPndftrnUiEvent(): InsertPendaftranUiEvent = InsertPendaftranUiEvent(
+    id_pendaftaran = id_pendaftaran,
+    id_kursus = id_kursus,
+    id_siswa = id_siswa,
+    tanggal_pendaftaran = tanggal_pendaftaran
+)
